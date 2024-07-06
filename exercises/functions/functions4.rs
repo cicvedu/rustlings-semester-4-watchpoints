c6e1b1c0f11309a50b9f5fd61e0346ca996c6b4c
程序员小王(troy) troy <wang_cyi@163.com>
2024-07-06 15:41:03 +0000

    if

--- exercises/functions/functions4.rs
+++ exercises/functions/functions4.rs
@@ -8,19 +8,19 @@
 // Execute `rustlings hint functions4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
     let original_price = 51;
     println!("Your sale price is {}", sale_price(original_price));
 }
 
-fn sale_price(price: i32) -> {
+fn sale_price(price: i32) ->i32 {
     if is_even(price) {
         price - 10
     } else {
         price - 3
     }
+    //在 Rust 中，如果函数的最后一个表达式是一个没有被分号 `;` 终止的表达式，那么这个表达式的值将被用作函数的返回值
 }
 
 fn is_even(num: i32) -> bool {
