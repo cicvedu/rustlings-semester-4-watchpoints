b1858b4a6523fdaaa22c578a72f56dee8a82a2b5
程序员小王(troy) troy <wang_cyi@163.com>
2024-07-07 04:14:26 +0000

    optional_target

--- exercises/options/options1.rs
+++ exercises/options/options1.rs
@@ -3,7 +3,7 @@
 // Execute `rustlings hint options1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 // This function returns how much icecream there is left in the fridge.
 // If it's before 10PM, there's 5 pieces left. At 10PM, someone eats them
@@ -13,7 +13,8 @@ fn maybe_icecream(time_of_day: u16) -> Option<u16> {
     // value of 0 The Option output should gracefully handle cases where
     // time_of_day > 23.
     // TODO: Complete the function body - remember to return an Option!
-    ???
+    let icecreams = if time_of_day < 22 { 5 } else if time_of_day < 24 { 0 } else { return None };
+    Some(icecreams)
 }
 
 #[cfg(test)]
