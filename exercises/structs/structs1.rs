1ee7e260207ab4f6e6aa6b591af0b6379056fc5e
程序员小王(troy) troy <wang_cyi@163.com>
2024-07-07 03:54:32 +0000

    hashmaps3

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -53,6 +53,7 @@ mod tests {
     fn unit_structs() {
         // TODO: Instantiate a unit-like struct!
         // let unit_like_struct =
+        let unit_like_struct = UnitLikeStruct;
         let message = format!("{:?}s are fun!", unit_like_struct);
 
         assert_eq!(message, "UnitLikeStructs are fun!");
